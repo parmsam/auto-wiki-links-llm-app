79ce97e33d0be4a36954348d001831685e7e08b3
parmsam <parmartsam@gmail.com>
2024-09-08 21:07:49 -0400

    reformat default text

--- app.py
+++ app.py
@@ -18,12 +18,7 @@ each keyword. If a Wikipedia page exists, the keyword is replaced with
 a link to the Wikipedia page. The text is markdown formatted. 
 """
 
-default_text = """
-Evangelion is set 15 years after a worldwide cataclysm in the futuristic 
-fortified city of Tokyo-3. The protagonist is Shinji Ikari, a teenage boy 
-recruited by his father Gendo to the mysterious organization Nerv. Shinji 
-must pilot an Evangelion, a giant biomechanical mecha, and fight beings known 
-as Angels.
+default_text = f"""Evangelion is set 15 years after a worldwide cataclysm in the futuristic fortified city of Tokyo-3. The protagonist is Shinji Ikari, a teenage boy recruited by his father Gendo to the mysterious organization Nerv. Shinji must pilot an Evangelion, a giant biomechanical mecha, and fight beings known as Angels.
 """
     
 app_ui = ui.page_fluid(
@@ -36,8 +31,7 @@ app_ui = ui.page_fluid(
     ui.input_text_area(
         "text_input", 
         "Enter your text:", 
-        rows=5, 
-        width = "100%",
+        rows=5, width = "100%",
         value = default_text,
     ),
     ui.output_ui("markdown_output"),
